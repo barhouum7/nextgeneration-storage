a96d24437b0320b33f1068801213ed0b96b2eed1
IbrahimBHMBS <bb.med2000@gmail.com>
2022-03-21 23:15:37 +0100

    Feat: Added the Main Class

---
+++ src/components/Main.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+// import { convertBytes } from './helpers';
+// import moment from 'moment';
+
+class Main extends Component {
+    render() {
+        return (
+            <div classMame="container-fluid mt-5 text-center">
+                <div className="row">
+                    <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '1024px' }}>
+                        <div className="content">
+                            <p>&nbsp;</p>
+                            <h1>A decentralized solution for storing our clients' documents in a secure and transparent manner.</h1>
+                        </div>
+                    </main>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Main;

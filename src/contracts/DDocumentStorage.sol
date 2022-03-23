3bf02f356dd15475fd0a9c3f1fb61f77486f8e0d
IbrahimBHMBS <bb.med2000@gmail.com>
2022-03-23 23:04:36 +0100

    Feat: Contract events

--- src/contracts/DDocumentStorage.sol
+++ src/contracts/DDocumentStorage.sol
@@ -1,9 +1,36 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.6.0 <0.9.0;
 
 contract DDocumentStorage {
     string public name = 'DDocumentStorage';
     uint public fileCount = 0;
     mapping(uint => File) public files;
 
+    struct File {
+        uint fileId;
+        string fileHash;
+        uint fileSize;
+        string fileType;
+        string fileName;
+        string fileDescription;
+        uint uploadTime;
+        address payable uploader;
+    }
+
+    event FileUploaded(
+        uint fileId,
+        string fileHash,
+        uint fileSize,
+        string fileType,
+        string fileName,
+        string fileDescription,
+        uint uploadTime,
+        address payable uploader
+    );
+
+    constructor() public {
+
+    }
+
     
+
 }

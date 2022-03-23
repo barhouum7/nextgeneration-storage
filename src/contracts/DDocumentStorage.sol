b6012bd907a730a5dc92fd6683b5d25ceb7db817
IbrahimBHMBS <bb.med2000@gmail.com>
2022-03-23 23:57:34 +0100

    Feat: Added uplaodFile Function

--- src/contracts/DDocumentStorage.sol
+++ src/contracts/DDocumentStorage.sol
@@ -27,10 +27,29 @@ contract DDocumentStorage {
         address payable uploader
     );
 
-    constructor() public {
+    constructor () public {
 
     }
+    function uploadFile (string memory _fileHash, uint _fileSize, string memory _fileType, string memory _fileName, string memory _fileDescription) public {
+        // Make sure the file hash exists
+        require(bytes(_fileHash).length > 0);
+        // MAke sure file Type exists
+        require(bytes(_fileType).length > 0);
+        // MAke sure file Description exists
+        require(bytes(_fileDescription).length > 0);
+        // MAke sure file Name exists
+        require(bytes(_fileName).length > 0);
+        // MAke sure uploader address exists
+        require(msg.sender != address(0));
+        // MAke sure file size is more than 0
+        require(_fileSize > 0);
 
-    
+        // Increment file id
+        fileCount++;
 
+        // Add File to the contract
+        files[fileCount] = File(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender);
+        // Trigger an Event
+        emit FileUploaded(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender);
+    }
 }

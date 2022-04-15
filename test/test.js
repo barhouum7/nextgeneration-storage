0c11128d6a6613725778c86d3d79598c0e5908ce
IbrahimBHMBS <bb.med2000@gmail.com>
2022-04-15 05:34:23 +0100

    Feat: Setup Tests

---
+++ test/test.js
@@ -0,0 +1,11 @@
+const { assert } = require('chai');
+
+const DDocumentStorage = artifacts.require('./DDocumentStorage');
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+contract('DDocumentStorage', ([deployer, uploader]) => {
+
+})

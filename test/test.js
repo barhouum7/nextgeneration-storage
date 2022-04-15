14406498ffe35984794d0f212dce08bd5a86b3a2
IbrahimBHMBS <bb.med2000@gmail.com>
2022-04-15 05:37:05 +0100

    Feat: Tests >> Deployment

--- test/test.js
+++ test/test.js
@@ -7,5 +7,25 @@ require('chai')
     .should()
 
 contract('DDocumentStorage', ([deployer, uploader]) => {
+    let ddstorage
+
+    before(async () => {
+        ddstorage = await DDocumentStorage.deployed()
+    })
+
+    describe('deployment', async () => {
+        it('deploys successfully', async () => {
+            const address = await ddstorage.address
+            assert.notEqual(address, 0x0)
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+        })
+
+        it('has a name', async () => {
+            const name = await ddstorage.name()
+            assert.equal(name, 'DDocumentStorage')
+        })
+    })
 
 })

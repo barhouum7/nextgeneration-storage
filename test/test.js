44c1d40ebe87ec5efef336e24de1a9d581f3e606
IbrahimBHMBS <bb.med2000@gmail.com>
2022-04-15 05:40:29 +0100

    Feat: Tests >> Check the event Upload File

--- test/test.js
+++ test/test.js
@@ -28,4 +28,43 @@ contract('DDocumentStorage', ([deployer, uploader]) => {
         })
     })
 
+    describe('file', async () => {
+        let result, fileCount
+        const fileHash = ''
+        const fileSize = '1'
+        const fileType = 'TypeOfTheFile'
+        const fileName = 'NameOfTheFile'
+        const fileDescription = 'DescriptionOfTheFile'
+
+        before(async () => {
+            result = await ddstorage.uploadFile(fileHash, fileSize, fileType, fileName, fileDescription, { from: uploader })
+            fileCount = await ddstorage.fileCount()
+        })
+
+        // Check the event
+        it('upload file', async () => {
+            // If Success
+            assert.equal(fileCount, 1)
+            const event = result.logs[0].args
+            assert.equal(event.fileId.toNumber(), fileCount.toNumber(), 'Id is correct!')
+            assert.equal(event.fileHash, fileHash, 'Hash is correct!')
+            assert.equal(event.fileSize, fileSize, 'Size is correct!')
+            assert.equal(event.fileType, fileType, 'Type is correct!')
+            assert.equal(event.fileName, fileName, 'Name is correct!')
+            assert.equal(event.fileDescription, fileDescription, 'Description is correct!')
+            assert.equal(event.uploader, uploader, 'Uploader is correct!')
+
+            // FAILURE: File must have Hash
+            await ddstorage.uploadFile('', fileSize, fileType, fileName, fileDescription, { from: uploader }).should.be.rejected;
+            // FAILURE: File must have Size
+            await ddstorage.uploadFile(fileHash, '', fileType, fileName, fileDescription, { from: uploader }).should.be.rejected;
+            // FAILURE: File must have Type
+            await ddstorage.uploadFile(fileHash, fileSize, '', fileName, fileDescription, { from: uploader }).should.be.rejected;
+            // FAILURE: File must have Name
+            await ddstorage.uploadFile(fileHash, fileSize, fileType, '', fileDescription, { from: uploader }).should.be.rejected;
+            // FAILURE: File must have Description
+            await ddstorage.uploadFile(fileHash, fileSize, fileType, fileName, '', { from: uploader }).should.be.rejected;
+        })
+
+    })
 })

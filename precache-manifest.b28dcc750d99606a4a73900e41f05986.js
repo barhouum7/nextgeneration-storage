f9447298b0c9a10579a5bc2fee758a93c1edd5a4
IbrahimBHMBS <bb.med2000@gmail.com>
2022-04-20 06:42:29 +0100

    Deploy React app to GitHub Pages

---
+++ precache-manifest.b28dcc750d99606a4a73900e41f05986.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4be1f7f9e7ac853b78d54e0ed9f2f8d0",
+    "url": "/nextgeneration-storage/index.html"
+  },
+  {
+    "revision": "b9f62c760b97acd99de6",
+    "url": "/nextgeneration-storage/static/css/2.7b4682b0.chunk.css"
+  },
+  {
+    "revision": "b9f62c760b97acd99de6",
+    "url": "/nextgeneration-storage/static/js/2.6e9fa14d.chunk.js"
+  },
+  {
+    "revision": "231cd9d7cdd0e5a2c79a68f3333d30a7",
+    "url": "/nextgeneration-storage/static/js/2.6e9fa14d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f4cbcf50ba0271362a5",
+    "url": "/nextgeneration-storage/static/js/main.0222863d.chunk.js"
+  },
+  {
+    "revision": "99d797c86b0d2564f72d",
+    "url": "/nextgeneration-storage/static/js/runtime-main.45050e88.js"
+  },
+  {
+    "revision": "df8b84d5c24a3544c25001f3959d7a77",
+    "url": "/nextgeneration-storage/static/media/box.df8b84d5.png"
+  }
+]);

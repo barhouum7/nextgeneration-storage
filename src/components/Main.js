d45b6a1a81659406c95c1b6b361d5188b2398e1b
IbrahimBHMBS <bb.med2000@gmail.com>
2022-04-20 06:35:59 +0100

    Fix: fixed some typos, styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,9 +5,9 @@ import React, { Component } from 'react';
 class Main extends Component {
     render() {
         return (
-            <div classMame="container-fluid mt-5 text-center">
+            <div className="container-fluid mt-5 text-center" style={{ width: '1248px' }}>
                 <div className="row">
-                    <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '1024px' }}>
+                    <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '900px' }}>
                         <div className="content">
                             <p>&nbsp;</p>
                             <h1>A decentralized solution for storing our clients' documents in a secure and transparent manner.</h1>

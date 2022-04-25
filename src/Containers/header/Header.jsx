3dd4179948e14c85604ffd61e627ad5103ca21f1
IbrahimBHMBS <bb.med2000@gmail.com>
2022-04-25 22:39:35 +0100

    Feat: Added a Header Component

--- src/Containers/header/Header.jsx
+++ src/Containers/header/Header.jsx
@@ -3,7 +3,12 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header section__padding' id='home'>
+      <div className='header-content'>
+      <h1 className='gradient__text'>A decentralized solution for storing our clients' documents in a secure and transparent manner.</h1>
+
+      </div>
+    </div>
   )
 }
 

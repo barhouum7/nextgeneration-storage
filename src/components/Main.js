365fdc01d32bb091aef189b35e150548da102704
IbrahimBHMBS <bb.med2000@gmail.com>
2022-05-04 00:00:10 +0100

    Feat: Icluded my Containers into the Main Component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react';
 // import { convertBytes } from './helpers';
 // import moment from 'moment';
-// import { Header } from '../Containers'
+import { Footer, UploadFileForm, InfoTable } from '../Containers'
 
 class Main extends Component {
     render() {
         return (
-            <div>
-                <div className="row">
-                    <main role="main">
-                        <div className="content">
-                            <p>&nbsp;</p>
-                            <h1>A decentralized solution for storing our clients' documents in a secure and transparent manner.</h1>
-                        </div>
-                    </main>
-                </div>
+            <div className='content'>
+                <UploadFileForm />
+                <InfoTable />
+                <Footer />
             </div>
         );
     }

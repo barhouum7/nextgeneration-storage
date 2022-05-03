2a035dbc08bdd35f28ef01822cf84f3d9ac501b4
IbrahimBHMBS <bb.med2000@gmail.com>
2022-05-03 23:58:33 +0100

    Feat: Added index file for all Containers

--- src/Containers/index.js
+++ src/Containers/index.js
@@ -1,1 +1,4 @@
 export { default as Header } from './header/Header'
+export { default as Footer } from './footer/Footer'
+export { default as UploadFileForm } from './uploads/UploadFileForm'
+export { default as InfoTable } from './infotable/InfoTable'

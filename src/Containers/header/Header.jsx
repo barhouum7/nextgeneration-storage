4b317fe237f2d441a8f10ca55726c055d5032212
IbrahimBHMBS <bb.med2000@gmail.com>
2022-05-09 05:08:12 +0100

    Feat: Added a Header ai Image

--- src/Containers/header/Header.jsx
+++ src/Containers/header/Header.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './header.css'
+import ai from '../../assets/ai.png'
+
 
 const Header = () => {
   return (
     <div className='header section__padding' id='home'>
       <div className='header-content'>
-      <h1 className='gradient__text'>A decentralized solution for storing our clients' documents in a secure and transparent manner.</h1>
-
+        <h1 className='gradient__text'>A decentralized solution for storing our clients' documents in a secure and transparent manner.</h1>
+        <p>The Future is Now and You Just Need To Realize it. Step into Future Today & Make it Happen.</p>
+      </div>
+      <div className='header-image'>
+        <img src={ai} alt="ai" />
       </div>
     </div>
   )

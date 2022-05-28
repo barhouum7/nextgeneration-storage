1e51e6da07e37a56c4b2463fdf4cbef4e8c77c1d
IbrahimBHMBS <bb.med2000@gmail.com>
2022-05-28 13:53:05 +0100

    Feat: Added Moralis Provider wrap

--- src/index.js
+++ src/index.js
@@ -5,7 +5,19 @@ import MyApp from './MyApp';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<MyApp />, document.getElementById('root'));
+import { MoralisProvider } from "react-moralis"
+
+ReactDOM.render(
+    <React.StrictMode>
+    <MoralisProvider
+        serverUrl="process.env.REACT_PUBLIC_MORALIS_SERVER"
+        appId="process.env.REACT_PUBLIC_MORALIS_APP_ID"
+    >
+        <MyApp />
+    </MoralisProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
